389fd4037a29a073a00935525090f3569c90446e
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-12-12 11:59:09 +0700

    create: profile frag

--- app/src/main/java/com/rizkyizh/panopticon/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/rizkyizh/panopticon/ui/profile/ProfileViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "fitur ini belum tersedia"
     }
     val text: LiveData<String> = _text
 }
