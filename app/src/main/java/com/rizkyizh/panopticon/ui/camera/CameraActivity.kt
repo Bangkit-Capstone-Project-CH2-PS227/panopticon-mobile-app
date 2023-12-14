b0d5e067854c4cfad4fd7f92f8f02c5c81f541a3
rizkyizh <rizkiizzulhaq14@gmail.com>
2023-12-14 14:56:17 +0700

    create: camera activity

---
+++ app/src/main/java/com/rizkyizh/panopticon/ui/camera/CameraActivity.kt
@@ -0,0 +1,64 @@
+package com.rizkyizh.panopticon.ui.camera
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.rizkyizh.panopticon.R
+import com.rizkyizh.panopticon.databinding.ActivityCameraBinding
+
+class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCameraBinding
+
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        startCamera()
+
+        binding.btnExit.setOnClickListener {
+            finish()
+        }
+    }
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview
+                )
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    this@CameraActivity,
+                    "Gagal memunculkan kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e(TAG, "startCamera: ${exc.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    companion object {
+        private const val TAG = "CameraActivity"
+    }
+}
